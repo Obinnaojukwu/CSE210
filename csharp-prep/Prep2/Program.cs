3ec6e8ae31cc0e9eb189db0930b2542fd6149c63
Obinnaojukwu <gd064604@gmail.com>
2024-09-21 11:54:35 +0100

    C# week 1 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Whats your grade ? ");
+        string Grade = Console.ReadLine();
+
+        int Grades = int.Parse(Grade);
+
+        string letter = "";
+        string signs = "";
+
+        if (Grades >= 90)
+        {
+            letter = "A";
+            Console.WriteLine("You got an A");
+        }
+        else if (Grades >= 80)
+        {
+            letter = "B";
+           Console.WriteLine("You got a B");
+        }
+        else if (Grades >= 70)
+        {
+            letter = "C";
+            Console.WriteLine("You got a C");
+        }
+        else if (Grades >= 60)
+        {
+            letter = "D";
+            Console.WriteLine("You got a D");
+        }
+        else 
+        {
+            letter = "F";
+            Console.WriteLine("You got an F");
+        }
+
+        if (Grades >= 70)
+        {
+            Console.WriteLine("Congratulations! You PASSED the course");
+        }
+        else 
+        {
+            Console.WriteLine("You FAILED this course. Try again next semester");
+        }
+
+        int courseSign = Grades % 10;
+        if (Grades != 90 && Grades <= 70 )
+        {
+            if (courseSign >= 7)
+            {
+                signs = "+";
+            }
+            else if (courseSign < 3)
+            {
+                signs = "-";
+            }
+        }
+
+        if (Grades >= 90)
+        {
+            signs = "";
+        } 
+        if (Grades <= 70)
+        {
+            signs = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{signs}");
+
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int RandomNum = -1;
+        while (RandomNum != 0)
+        {
+            Console.Write("Enter number:  ");
+            RandomNum = int.Parse(Console.ReadLine());
+
+    
+            
+            if (RandomNum != 0)
+            {
+                numbers.Add(RandomNum);
+            }
+            
+        }
+        int addUp = 0;
+        int averageNum = numbers[0];
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            // Console.WriteLine(numbers[i]);
+            addUp += numbers[i];
+            if (numbers[i] > averageNum)
+            {
+                averageNum = numbers[i];
+            }
+        }
+        double average = (double)addUp / numbers.Count;
+
+        Console.WriteLine($"The sum is: {addUp}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest Number is :{averageNum}");
     }
 }
