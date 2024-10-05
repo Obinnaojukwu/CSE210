ca0845d764f85c753a1a8e66d4c448c404c28c04
Obinnaojukwu <gd064604@gmail.com>
2024-10-05 11:01:45 +0100

    scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,156 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all thine heart and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to countinue or type 'quit' to finish: ");
+
+            
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            else
+            {
+                scripture.HideRandomWords();
+            }
+            
+            if (scripture.AllWordsHidden())
+            {
+                Console.WriteLine("");
+
+                break;
+            }
+        }
+    }
+}
+
+class Scripture
+{
+    private Reference reference;
+    private List<Word> words;
+
+    public Scripture(Reference reference, string text)
+    {
+        this.reference = reference;
+        words = new List<Word>();
+        string[] wordArray = text.Split(' ');
+        foreach (string word in wordArray)
+        {
+            words.Add(new Word(word));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = reference.GetDisplayText() + " - ";
+        foreach (Word word in words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+        return scriptureText.Trim();
+    }
+
+    public void HideRandomWords()
+    {
+        Random rand = new Random();
+        int wordsToHide = rand.Next(1, 4); 
+
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int index = rand.Next(words.Count);
+            words[index].Hide();
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Reference
+{
+    private string book;
+    private int chapter;
+    private int startVerse;
+    private int endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.startVerse = verse;
+        this.endVerse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.startVerse = startVerse;
+        this.endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (startVerse == endVerse)
+        {
+            return $"{book} {chapter}:{startVerse}";
+        }
+        else
+        {
+            return $"{book} {chapter}:{startVerse}-{endVerse}";
+        }
+    }
+}
+
+class Word
+{
+    private string text;
+    private bool hidden;
+
+    public Word(string text)
+    {
+        this.text = text;
+        hidden = false;
+    }
+
+    public void Hide()
+    {
+        hidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return hidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (hidden)
+        {
+            return "____";
+        }
+        else
+        {
+            return text;
+        }
     }
 }
