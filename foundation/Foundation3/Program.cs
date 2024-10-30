8549495adf339e4b2bbea870dfbd2fe7869fa86f
Obinnaojukwu <gd064604@gmail.com>
2024-10-30 21:48:33 +0100

    tracker

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,86 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    private string date;
+    private int minutes;
+
+    public Activity(string date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public string Date => date;
+    public int Minutes => minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{Date} ({Minutes} min) - Distance: {GetDistance():F1} km, Speed: {GetSpeed():F1} kph, Pace: {GetPace():F2} min per km";
+    }
+}
+
+class Running : Activity
+{
+    private double distance;
+
+    public Running(string date, int minutes, double distance) : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance() => distance;
+    public override double GetSpeed() => (distance / Minutes) * 60;
+    public override double GetPace() => Minutes / distance;
+}
+
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(string date, int minutes, double speed) : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance() => (speed * Minutes) / 60;
+    public override double GetSpeed() => speed;
+    public override double GetPace() => 60 / speed;
+}
+
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(string date, int minutes, int laps) : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance() => (laps * 50) / 1000.0;
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
+    public override double GetPace() => Minutes / GetDistance();
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("03 Nov 2022", 30, 9.7),
+            new Swimming("03 Nov 2022", 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
